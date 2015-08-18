a15f283b31a218c16abdc4e2c65bc5469cbc0285
Rostech <rostislavkazarin@gmail.com>
2015-08-18 21:35:42 +0300

    Latest homework update

---
+++ High-Quality Code/15. Development Tools/Homework/T4TemplateExample/T4TemplateExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4TemplateExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var templateTester = new TestTemplate();
+            templateTester.Creeting();
+        }
+    }
+}
---
+++ High-Quality Code/15. Development Tools/Homework/T4TemplateExample/T4TemplateExample/template.cs
@@ -0,0 +1,22 @@
+﻿namespace T4TemplateExample
+{
+	public class TestTemplate
+	{
+		private void Tuesday()
+		{
+			System.Console.WriteLine("Today is {0}", System.DateTime.Now.DayOfWeek);
+		}
+	
+		public void Creeting()
+		{
+			string ho = "";
+								ho += "Ho, ";
+									ho += "Ho, ";
+					
+			ho += "Ho. :) ";
+	
+			System.Console.WriteLine("Hello, World. {0}", ho);
+			Tuesday();
+		}
+	}
+}
